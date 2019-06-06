938c76ccadc318f40d5b8c2decefaa32250a8715
siriuz <siriuzzzz@mail.ru>
2019-06-06 09:12:19 +0200

    Unity tryout project

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float axel = 0.001f;
+    private float a = 1f;
+    public float gravity = -9.8f;
+    CharacterController body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float deltaX = 0;// Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        body.Move(movement);
+
+
+        //a = a + axel;
+        //transform.Translate(0, 0, speed * Input.GetAxis("Vertical") * Time.deltaTime * a);
+    }//speed * Input.GetAxis("Vertical") * Time.deltaTime
+}
---
+++ Assets/Scripts/ObstacleBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ObstacleBuilder : MonoBehaviour
+{
+    [SerializeField] private GameObject obstaclePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        float x0 = 0;
+        float y0 = -20;//transform.position.y;
+        float R = 20f; //transform.localScale.y/2;
+        
+        //for (float x = -R; x < R; x+=2*R/50)
+        for (int i=0;i<50;i++)
+        {
+            
+
+
+            //y = y0 + Mathf.Sqrt(R * R - x * x);
+            //obstacle.transform.position = new Vector3(x, y, 10);
+            int count = Random.Range(1, 4);
+            for(int j = 1; j <= count; j++)
+            {
+                GameObject obstacle = Instantiate(obstaclePrefab) as GameObject;
+                float u = Random.Range(-45f, 45f);
+                obstacle.transform.Rotate(0, 0, u);
+                obstacle.transform.position = new Vector3(0, 0, 10f + 6f * i);
+                /*float z = 6f * i;
+                float u = Random.Range(-45f, 45f);
+                float x = R * Mathf.Sin(u) + x0;
+                float y = R - R * Mathf.Cos(u) + y0;
+                Debug.Log("x=" + x + ",y=" + y + "z=" + z);
+                obstacle.transform.position = new Vector3(x, y, 10f + z);*/
+                obstacle.transform.SetParent(transform);
+            }
+            
+            //obstacle.transform.rotation = new Quaternion(0, Random.Range(0,0.2f), 0, 1);
+            //obstacle.transform.localEulerAngles = new Vector3(0, Random.Range(-0.2f, 0.2f), 0);
+
+            
+         //   
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
